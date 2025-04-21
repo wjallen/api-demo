ee1bb2b78c5a27beb7d547aee198de9a0aeb40dc
Joe Allen <wallen@tacc.utexas.edu>
2025-04-21 21:55:52 +0000

    removed some tests

--- test/test_api.py
+++ test/test_api.py
@@ -1,4 +1,4 @@
-
+#!/usr/bin/python3
 
 import json
 import os
@@ -10,41 +10,9 @@ import requests
 
 
 api_host = 'localhost'
-api_port = '5041'
+api_port = '5000'
 api_prefix = f'http://{api_host}:{api_port}'
 
-
-def test_info():
-    route = f'{api_prefix}/'
-    response = requests.get(route)
-
-    assert response.ok == True
-    assert response.status_code == 200
-    assert bool(re.search('Try the following routes', response.text)) == True
-
-
-def test_data_upload():
-    route = f'{api_prefix}/data'
-    response = requests.post(route)
-
-    assert response.ok == True
-    assert response.status_code == 200
-    assert response.content == b'Data has been loaded to Redis from file\n'
-
-
-def test_data_read():
-    route = f'{api_prefix}/data'
-    response = requests.get(route)
-
-    assert response.ok == True
-    assert response.status_code == 200
-
-    assert isinstance(response.json(), list) == True
-    assert isinstance(response.json()[0], dict) == True
-    assert 'name' in response.json()[0].keys()
-    assert 'id' in response.json()[0].keys()
-    assert 'reclat' in response.json()[0].keys()
-    assert 'mass (g)' in response.json()[0].keys()
 
 
 def test_jobs_info():
